10b84ccc6663a6b8bb260953fa8c0ecaba81199c
otonielnn <otoniel_2002@live.com>
2024-04-20 00:28:07 -0300

    add categorias module

--- workout_api/categorias/models.py
+++ workout_api/categorias/models.py
@@ -1,6 +1,5 @@
 from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from workout_api.atleta.models import AtletaModel
 from workout_api.contrib.models import BaseModel
 
 
--- workout_api/categorias/schemas.py
+++ workout_api/categorias/schemas.py
@@ -1,9 +1,13 @@
 from typing import Annotated
-from pydantic import Field
+from pydantic import UUID4, Field
 from workout_api.contrib.schemas import BaseSchema
 
 
-class Categoria(BaseSchema):
+class CategoriaIn(BaseSchema):
     nome: Annotated[
-        str, Field(description="Nome da categoria", example="Scale", max_length=50)
+        str, Field(description="Nome da categoria", example="Scale", max_length=10)
     ]
+
+
+class CategoriaOut(CategoriaIn):
+    id: Annotated[UUID4, Field(description="Identificador de categoria")]
