69a6ea407060ee4cd1611375e8a68ec78595f6a7
otonielnn <otoniel_2002@live.com>
2024-04-21 03:11:48 -0300

    add atleta endpoints e testes da API

--- workout_api/atleta/schemas.py
+++ workout_api/atleta/schemas.py
@@ -1,5 +1,7 @@
-from typing import Annotated
+from typing import Annotated, Optional
 from pydantic import Field, PositiveFloat
+from workout_api.categorias.schemas import CategoriaIn
+from workout_api.centro_treinamento.schemas import CentroTreinamentoAtleta
 from workout_api.contrib.schemas import BaseSchema, OutMixin
 
 
@@ -15,8 +17,10 @@ class Atleta(BaseSchema):
     altura: Annotated[
         PositiveFloat, Field(description="Altura do atleta", example="1.75")
     ]
-    altura: Annotated[
-        str, Field(description="Sexo do atleta", example="M", max_length=1)
+    sexo: Annotated[str, Field(description="Sexo do atleta", example="M", max_length=1)]
+    categoria: Annotated[CategoriaIn, Field(description="Categoria do atleta")]
+    centro_treinamento: Annotated[
+        CentroTreinamentoAtleta, Field(description="Centro de Treinamento do atleta")
     ]
 
 
@@ -26,3 +30,21 @@ class AtletaIn(Atleta):
 
 class AtletaOut(Atleta, OutMixin):
     pass
+
+
+class AtletaUpdate(BaseSchema):
+    nome: Annotated[
+        Optional[str],
+        Field(None, description="Nome do atleta", example="Joao", max_length=50),
+    ]
+    idade: Annotated[
+        Optional[int], Field(None, description="Idade do atleta", example="18")
+    ]
+    peso: Annotated[
+        Optional[PositiveFloat],
+        Field(None, description="Peso do atleta", example="81.4"),
+    ]
+    altura: Annotated[
+        Optional[PositiveFloat],
+        Field(None, description="Altura do atleta", example="1.75"),
+    ]
